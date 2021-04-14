3d95b04a9fc8d3b502387569b40726b4183bb03e
bongseok.oh <bongseok.oh@lge.com>
2021-04-14 15:19:51 +0900

    test

--- manual/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-# 파일을 업로드 하기 위한 Form Type 정의
-class FileListForm(forms.Form):
-    tester = forms.CharField(max_length=255)
-    origin_file = forms.FileField(label='PPTX or HTML File')
-    desc = forms.CharField(max_length=255)
